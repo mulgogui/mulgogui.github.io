c7a967773519deaa9f2b42b30b893f613db9b2ec
mulgogui <126698063+mulgogui@users.noreply.github.com>
2023-03-01 19:39:31 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+document.getElementById('boton_color').addEventListener('click', function() {
+        document.body.style.backgroundColor = 'black';
+        })
+
+document.getElementById('boton_default').addEventListener('click', function() {
+            document.body.style.backgroundColor = '#FFFEFD';
+            })
